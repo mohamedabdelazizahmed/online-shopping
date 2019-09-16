d7f888be32d8c06ecc4bdf73101a2b415b7f2cc7
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2019-09-16 15:29:37 +0200

    MVC :  fetching-data-from-files

--- models/product.js
+++ models/product.js
@@ -1,15 +1,42 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    console.log( path.dirname(process.mainModule.filename));
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    console.log(path.dirname(process.mainModule.filename));
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
