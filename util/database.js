c9e912058c2d9216c3d5e06b38f11c85b2035460
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-06 00:04:08 +0200

    1-Connecting to the Database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2');
-
-// create connection with pool 
-//  pool finished when application shutdown 
-const  pool =  mysql.createPool({
-    host : 'localhost',
-    user:'root',
-    database: 'node-complete',
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('node-complete', 'root', '', {
+  dialect: 'mysql',
+  host:'localhost'
 });
-module.exports = pool.promise();
+
+module.exports = sequelize;
