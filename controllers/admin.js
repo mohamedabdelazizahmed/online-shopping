0bb000ade19530a44a0b4283979cec090684e14f
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-12 19:16:24 +0200

    7-Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,18 +68,22 @@ exports.postEditProduct = (req, res, next) => {
 
       return product.save();
     })
-    .then(result => {
+    .then((result) => {
       console.log("... UPDATED PRODUCT ...");
-      res.redirect('/admin/products');
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
-
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findOneAndRemove(prodId)
+    .then(() => {
+        console.log("DESTROYED PRODUCT");
+        res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+
 };
 exports.getProducts = (req, res, next) => {
   Product.find()
