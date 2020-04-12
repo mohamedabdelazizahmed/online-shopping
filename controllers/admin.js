a3ac63d262edb0f76d06f9be193738ad78a48ea1
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-12 18:06:33 +0200

    3-Saving Data Through Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,9 +16,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log("CREATED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -54,15 +64,13 @@ exports.postEditProduct = (req, res, next) => {
     updatePrice
   );
   updatedProduct.save();
-  res.redirect('/admin/products');
+  res.redirect("/admin/products");
 };
-
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
-  res.redirect('/admin/products');
-
+  res.redirect("/admin/products");
 };
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
