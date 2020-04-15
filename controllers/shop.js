a2c156dccf5330b2eaee71409f5ad3873ff5a483
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-15 11:06:12 +0200

    12-Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,10 +76,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .removeFromCart(prodId)
-    .then((result) => {
-        res.redirect("/cart");
+    .then(result => {
+      res.redirect('/cart');
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
