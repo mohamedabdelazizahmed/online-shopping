b77d0a6394ca938f0f439e7128c6e718a55d5a89
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-24 12:27:09 +0200

    4-Setting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,30 @@
 exports.getLogin = (req, res, next) => {
-  console.log("Login Page Render");  
+
+  // const isLoggedIn = req
+  //   .get('Cookie')
+  //   .split(';')[1]
+  //   .trim()
+  //   .split('=')[1];
+  console.log("Login Page Render");
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: req.isLoggedIn,
   });
 };
 
-
 exports.postLogin = (req, res, next) => {
-  /** store information isLoggedIn */ 
-  req.isLoggedIn = true;
-  res.redirect('/');
+  /** store information isLoggedIn */
+
+  //  req.isLoggedIn = true;
+  /**
+   * request is dead after sending response
+   * solution add cookie in header response
+   * The  cookie attached for every request
+   * The data  send for every request
+   *  open DEVTools in chrome > Application > Cookies
+   */
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+
+  res.redirect("/");
 };
